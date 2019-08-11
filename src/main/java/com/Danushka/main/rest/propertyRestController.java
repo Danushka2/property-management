9521c007d123af7e5c08f53aaedeca3b4dede7e8
Danushka2 <dhanushkanuwan24@gmail.com>
2019-08-12 02:51:54 +0530

    JPA is used instead of Hibernate

--- src/main/java/com/Danushka/main/rest/propertyRestController.java
+++ src/main/java/com/Danushka/main/rest/propertyRestController.java
@@ -3,23 +3,28 @@ package com.Danushka.main.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Danushka.main.CRUD.propertyDAO;
+import com.Danushka.main.CRUD.propertyRepository;
 import com.Danushka.main.entity.property;
 
+//@Controller
 @RestController
 @RequestMapping("/api")
 public class propertyRestController {
 
 	propertyDAO propertyDAOObject;
+	propertyRepository proJPA;
 	
 	@Autowired
-	public propertyRestController(propertyDAO propertyDAOObj) {
+	public propertyRestController(propertyDAO propertyDAOObj, propertyRepository obj) {
 		super();
 		this.propertyDAOObject = propertyDAOObj;
+		this.proJPA = obj;
 	}
 
 
@@ -37,8 +42,8 @@ public class propertyRestController {
 	
 	@GetMapping("/find")
 	public List<property> findProerty() {
-		
-		return propertyDAOObject.findAll();
+	
+		return proJPA.findAll();
 	}
 
 }
