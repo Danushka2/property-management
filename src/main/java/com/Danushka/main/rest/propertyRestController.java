d5d4291811557a6cf5294c9f7d61914bdd40186a
Danushka2 <dhanushkanuwan24@gmail.com>
2019-08-12 20:37:06 +0530

    findById implemented

--- src/main/java/com/Danushka/main/rest/propertyRestController.java
+++ src/main/java/com/Danushka/main/rest/propertyRestController.java
@@ -1,16 +1,17 @@
 package com.Danushka.main.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.Danushka.main.CRUD.propertyDAO;
 import com.Danushka.main.CRUD.propertyRepository;
 import com.Danushka.main.entity.property;
 
@@ -39,9 +40,22 @@ public class propertyRestController {
 	}
 	
 	@GetMapping("/property")
-	public List<property> findProerty() {
+	public List<property> findProerties() {
 	
 		return proJPA.findAll();
+	}
+	
+	@GetMapping("/property/{propertyId}")
+	public property findProerty(@PathVariable int propertyId) {
+	
+		property searchedProp = null;
+		Optional<property> results = proJPA.findById(propertyId);
+		
+		if(results.isPresent()){
+			searchedProp =  results.get();
+		}
+		
+		return searchedProp;
 	}
 
 }
