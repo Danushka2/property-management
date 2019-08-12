b2459dd5d1bda66caca8cbd3a494520efbb052a1
Danushka2 <dhanushkanuwan24@gmail.com>
2019-08-12 19:42:19 +0530

    save data using JPA

--- src/main/java/com/Danushka/main/rest/propertyRestController.java
+++ src/main/java/com/Danushka/main/rest/propertyRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ public class propertyRestController {
 
 	propertyDAO propertyDAOObject;
 	propertyRepository proJPA;
+	property obj;
 	
 	@Autowired
 	public propertyRestController(propertyDAO propertyDAOObj, propertyRepository obj) {
@@ -38,6 +41,12 @@ public class propertyRestController {
 		
 		propertyDAOObject.save();
 		return "entry saved";
+	}
+	
+	@PostMapping("/savejson")
+	public void saveProertyJSON(@RequestBody property propertyOBJ) {
+		this.obj = propertyOBJ;
+		proJPA.save(obj);
 	}
 	
 	@GetMapping("/find")
