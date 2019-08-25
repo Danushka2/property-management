3a2a095cb07deea2e2b6cf5e6a3f24c68229dec0
Danushka2 <dhanushkanuwan24@gmail.com>
2019-08-25 23:40:20 +0530

    removed all the old project data

--- src/main/java/com/Danushka/main/CRUD/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Danushka.main.CRUD;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Danushka.main.entity.Property;
-
-public interface PropertyRepository extends JpaRepository<Property, Integer> {
-
-}
