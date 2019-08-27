83afedbb88bf6cfa681aaca0f03d6dc0d410d339
Danushka2 <dhanushkanuwan24@gmail.com>
2019-08-27 13:42:17 +0530

    deleted

--- src/main/java/com/Boardimak/main/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Boardimak.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Boardimak.main.entity.Property;
-
-
-public interface PropertyRepository extends JpaRepository<Property, Integer> {
-
-}
