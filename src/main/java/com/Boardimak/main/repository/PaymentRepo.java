6354bf4a343de82d4fd2c2030265aaa70eafa9ff
Danushka2 <dhanushkanuwan24@gmail.com>
2019-10-06 01:12:16 +0530

    integrated project

--- src/main/java/com/Boardimak/main/repository/PaymentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Boardimak.main.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.Boardimak.main.model.Payments;
-
-public interface PaymentRepo extends CrudRepository<Payments, Integer>{
-
-}
